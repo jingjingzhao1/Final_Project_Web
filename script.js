23668f5b3dbe68f0a19ce910a3fa918e580bf6ae
jingjingzhao1 <jingjing.zhao001@gmail.com>
2023-03-15 11:15:42 -0700

    script: delete console log

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ console.log("hello!");
 var menu = document.querySelector("#menu-icon");
 var navlist = document.querySelector(".navlist");
 
-if (menu) {
-	console.log(menu);
-}
-if (navlist) {
-	console.log(navlist);
-}
-
-console.log(menu);
-console.log(navlist);
 menu.addEventListener("click", () => {
 	menu.classList.toggle("bx-x");
 	navlist.classList.toggle('show');
