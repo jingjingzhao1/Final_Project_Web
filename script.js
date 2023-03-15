9c5a8196252b53f0d378bb7758d47e76e40027cd
jingjingzhao1 <jingjing.zhao001@gmail.com>
2023-03-15 12:14:43 -0700

    Back to top function

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ menu.addEventListener("click", () => {
 	menu.classList.toggle("bx-x");
 	navlist.classList.toggle('show');
 });
+
+const backToTop = document.querySelector('#back-to-top');
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
